595c57674d70cd45588f424a76111af5f0b5bd0c
golgauth <gauthier.boaglio@cefe.cnrs.fr>
2018-03-13 11:09:11 +0100

    Removed sphinx python doc (using 'Epidoc').

--- DOXY/cyelp_sphinx/build_doc.sh
+++
@@ -1,6 +0,0 @@
-#export PYTHONPATH="$PYTHONPATH:./setup"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:../../ECLIPSE-WS_ELP/evolab-cython/lib"
-#make clean
-#sphinx-apidoc -o . . /home/golgauth/Documents/02_DEV/EVOLAB/evolab-cython
-make html
-
